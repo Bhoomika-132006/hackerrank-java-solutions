636b78f20d93a4717b6252d3975e847f8ae3c10f
Bhoomika-132006 <bm3365765@gmail.com>
2026-09-22 12:06:30 +0530

    changes

--- Java/compactors/solution.java
+++ Java/compactors/solution.java
@@ -1,10 +1,8 @@
 import java.util.*;
 
 // Write your Checker class here
-class Checker implements Comparator {
-    public int compare(Object o1,  Object o2) {
-        Player p1 = (Player) o1;
-        Player p2 = (Player) o2;
+class Checker implements Comparator<Player> {
+    public int compare(Player p1, Player p2) {
         if (p2.score - p1.score == 0) return p1.name.compareTo(p2.name);
         return p2.score - p1.score;
     }
